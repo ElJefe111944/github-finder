822aa51fea7437f4f63d9a1937d59c342c53ab43
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-04-18 00:46:33 +0100

    Daisy ui installed and theme added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
+      <h1>
         Hello world!
       </h1>
     </div>

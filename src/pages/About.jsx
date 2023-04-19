2d492bee3ed4193de0de47397f726fd751269260
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-04-19 07:27:01 +0100

    Content & styling added to NotFound and About components

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. The repository for this project can be found 
+        in my GitHub repos&nbsp; 
+        <strong>
+          <a className='underline' target='_blank' href='https://github.com/ElJefe111944/github-finder'>here</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Layout By: <strong className='text-white'>James Farr</strong>
+      </p>
+    </div>
   )
 }
 

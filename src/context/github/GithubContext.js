5d596763bd4fc1dc774184c3e93ae1ae795d9267
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-04-22 15:27:57 +0100

    useState hooks in Github context replaced with reducers

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,23 +7,28 @@ const GITHUB_URL= process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN= process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const initialState = {
+        users: [],
+        loading: true,
+    }
+
+    const [state, dipatch] = useReducer(githubReducer, initialState)
 
     const fetchUsers = async () => {
         const response = await fetch(`${GITHUB_URL}/users`, {
             Authorization: `token ${GITHUB_TOKEN}`
         })
         const data = await response.json();
-        console.log(data);
 
-        setLoading(false);
-        setUsers(data);
+        dipatch({
+            type: 'GET_USERS',
+            payload: data,
+        })
     }
 
     return <GithubContext.Provider value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers
     }}>
         {children}

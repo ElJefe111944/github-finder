b188053a466845072e559e68029baf5efcde955d
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-04-22 15:09:53 +0100

    Github context setup

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import UserItem from "./UserItem";
 import Spinner from '../layout/Spinner';
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
 
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-   
+  const { users, loading, fetchUsers } = useContext(GithubContext);
+  
     useEffect(() => {
         fetchUsers(); 
     }, []);
-
-    const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-        })
-        const data = await response.json();
-        console.log(data);
-
-        setLoading(false);
-        setUsers(data);
-    }
 
     if(!loading){
       return (

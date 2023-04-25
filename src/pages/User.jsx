147e63bf24262749822a7903dfe6dde8120fe4b6
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-04-25 08:00:48 +0100

    User stats added to User component along with relevant styling

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -74,8 +74,59 @@ function User({ }) {
                             </a>
                         </div>
                     </div>
+                    <div className="w-full rounded-lg-shadow-md bg-base-100 stats flex flex-col lg:flex-row">
+                        {location && (
+                            <div className="stat">
+                                <div className="stat-title text-md">Location</div>
+                                <div className="text-lg stat-value">{location}</div>
+                            </div>
+                        )}
+                        {blog && (
+                            <div className="stat">
+                                <div className="stat-title text-md">Website</div>
+                                <div className="text-lg stat-value"><a href={`https://${blog}`} rel="noreferrer" target="_blank">{blog}</a></div>
+                            </div>
+                        )}
+                        {twitter_username && (
+                            <div className="stat">
+                                <div className="stat-title text-md">Twitter</div>
+                                <div className="text-lg stat-value"><a href={`https://twitter.com/${twitter_username}`} rel="noreferrer" target="_blank">{twitter_username}</a></div>
+                            </div>
+                        )}
+                    </div>
                 </div>
+  
             </div>
+            <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats flex flex-col lg:flex-row">
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaUsers className="text-3xl md:text-5xl" />
+                        </div>
+                        <div className="stat-title pr-5">Followers</div>
+                        <div className="stat-valu pr-5 text-3xl md:text-4xl">{followers}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaUserFriends className="text-3xl md:text-5xl" />
+                        </div>
+                        <div className="stat-title pr-5">Following</div>
+                        <div className="stat-valu pr-5 text-3xl md:text-4xl">{following}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaCodepen className="text-3xl md:text-5xl" />
+                        </div>
+                        <div className="stat-title pr-5">Public Repos</div>
+                        <div className="stat-valu pr-5 text-3xl md:text-4xl">{public_repos}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <FaStore className="text-3xl md:text-5xl" />
+                        </div>
+                        <div className="stat-title pr-5">Public Gists</div>
+                        <div className="stat-valu pr-5 text-3xl md:text-4xl">{public_gists}</div>
+                    </div>
+                </div>
         </div>
     )
 }
